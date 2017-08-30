67ab38ccbfc545ce661cf4d0345e046ddec9cd4a
Spike2_Lin <Spike2_Lin@asus.com>
2017-08-30 17:32:48 +0800

    Compete to 14 question

--- all_exercises/12.py
+++ all_exercises/12.py
@@ -1,3 +1,26 @@
 #Create a script that generates a list whose items are products of the original list items multiplied by 10
 my_range = range(1, 21)
 print([10 * x for x in my_range])
+
+
+
+
+# 我的寫法比較不好, 這裡主要不會的是,  []  list的生成,
+# 能在[] 內寫expression,
+list1 = []
+for i in my_range:
+    list1.append( i * int(10) )
+print(list1)
+
+"""
+
+原來range產生出來的, 是int 物件
+>>> my = range(1,21)
+>>> my
+range(1, 21)
+>>> type(my)
+<class 'range'>
+>>> type(my[0])
+<class 'int'>
+>>>
+"""
--- all_exercises/13.py
+++ all_exercises/13.py
@@ -1,5 +1,20 @@
 #Create a script that converts all items of the range to strings
 my_range = range(1, 21)
 print(map(str, my_range))
-for i in map:
+
+
+# 這裡因為 map object不能直接取用,所以, 我用iter
+print("Rewrite")
+for i in iter(map(str, my_range)):
+    print(type(i))
     print(i)
+
+
+print("Rewrite2")
+print([ x for x in iter(map(str, my_range)) ])
+
+
+
+# I wrote
+print("spike")
+print([ str(x) for x in my_range ])
