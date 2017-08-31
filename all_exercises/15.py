3df25c656163c75de7c7189d5d69e6172b8acc97
Spike2_Lin <Spike2_Lin@asus.com>
2017-08-31 09:49:38 +0800

    question 15 complete

--- all_exercises/15.py
+++ all_exercises/15.py
@@ -1,2 +1,20 @@
 #Create a dictionary of two keys, a and b and two values 1 and 2 for keys a and b respectively.
 d = {"a": 1, "b": 2}
+
+
+
+"""
+
+{} 是建出set , 而且取用,只能用 pop func.
+>>> dic = {('a',1),('b',2)}
+>>> dic
+{('a', 1), ('b', 2)}
+>>> type(dic)
+<class 'set'>
+>>> d = dict(a = 1, b = 2)
+>>> d
+{'a': 1, 'b': 2}
+
+
+dict的物件的話, 印出來是有: , colon 這個符號.
+"""
