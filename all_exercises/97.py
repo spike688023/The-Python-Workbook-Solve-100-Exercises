8213793ebea35084cfa75e7a937ea71d004ac61a
Spike2_Lin <Spike2_Lin@asus.com>
2017-09-06 18:40:55 +0800

    Just comments

--- all_exercises/97.py
+++ all_exercises/97.py
@@ -1,5 +1,11 @@
 #Create a program that asks the user to submit text through a GUI
 
+"""
+我還以為, 它SAVE, 是用什麼方法, 
+
+原來是 close  存回去, 再用a mode 打開.
+"""
+
 file = open("user_data_save_close.txt", 'a+')
 
 while True:
